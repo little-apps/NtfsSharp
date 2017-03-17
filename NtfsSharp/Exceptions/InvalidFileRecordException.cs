ed83c8fdd571adc31fae5d94af3f1ec726697335
Nick H <nick@sameoldnick.com>
2017-03-16 23:10:05 -0600

    FileRecord object is included in InvalidFileRecordException

--- NtfsSharp/Exceptions/InvalidFileRecordException.cs
+++ NtfsSharp/Exceptions/InvalidFileRecordException.cs
@@ -1,19 +1,23 @@
 ﻿using System;
+using NtfsSharp.FileRecords;
 
 namespace NtfsSharp.Exceptions
 {
     class InvalidFileRecordException : Exception
     {
         public readonly string ParamName;
+        public readonly FileRecord FileRecord;
 
-        public InvalidFileRecordException(string paramName)
+        public InvalidFileRecordException(string paramName, FileRecord fileRecord)
         {
             ParamName = paramName;
+            FileRecord = fileRecord;
         }
 
-        public InvalidFileRecordException(string paramName, string message) : base(message)
+        public InvalidFileRecordException(string paramName, string message, FileRecord fileRecord) : base(message)
         {
             ParamName = paramName;
+            FileRecord = fileRecord;
         }
     }
 }
