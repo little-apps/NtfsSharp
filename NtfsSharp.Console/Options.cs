0988313b863e24e8a547115a26ef80892b16608b
Nick H <nick@sameoldnick.com>
2017-04-30 21:59:36 -0600

    Console can read using physical disk or partition

--- NtfsSharp.Console/Options.cs
+++ NtfsSharp.Console/Options.cs
@@ -8,8 +8,14 @@ namespace NtfsSharp.Console
         [Option('o', "output", HelpText = "File to output to. If omitted, it is outputted to console")]
         public string OutputFile { get; set; }
 
-        [ValueOption(0)]
-        public char Drive { get; set; } = 'C';
+        [OptionArray('d', "physical-drive", DefaultValue = new [] { "" }, HelpText = "Physical drive and partition index to open. If not specified, 'partition' is used.")]
+        public string[] PhysicalDrive { get; set; }
+
+        [Option('p', "partition", DefaultValue = 'C', HelpText = "Partition drive letter to open.")]
+        public char Drive { get; set; }
+
+        [Option("list-drives", HelpText = "Lists the physical drives")]
+        public bool ListPhysicalDrives { get; set; }
 
         [HelpOption]
         public string GetUsage()
