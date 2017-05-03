e292e8c68cf62a0d226f2cdba113b022d20b4bf6
Nick H <nick@sameoldnick.com>
2017-05-03 14:45:58 -0600

    Added NUnit tests

---
+++ NtfsSharp.Tests/Driver/DummyDriver.cs
@@ -0,0 +1,123 @@
+﻿using NtfsSharp.Drivers;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Threading;
+using NUnit.Compatibility;
+
+namespace NtfsSharp.Tests.Driver
+{
+    internal class DummyDriver : BaseDiskDriver
+    {
+        public const uint DriveSize = 5 * 1024 * 1024;
+        public const uint BytesPerSector = 512;
+        public const uint SectorsPerCluster = 8;
+
+        public const uint MasterFileTableLcn = 1;
+        
+        public Dictionary<long, BaseDriverPart> Parts { get; } = new Dictionary<long, BaseDriverPart>();
+
+        private long _currentOffset;
+
+        public DummyDriver()
+        {
+
+        }
+
+        public override long Move(long offset, MoveMethod moveMethod = MoveMethod.Begin)
+        {
+            long newOffset;
+
+            switch (moveMethod)
+            {
+                case MoveMethod.Begin:
+                    newOffset = offset;
+                    break;
+                case MoveMethod.Current:
+                case MoveMethod.End:
+                    newOffset = _currentOffset + offset;
+                    break;
+                default:
+                    throw new ArgumentException("MoveMethod is not valid", nameof(moveMethod));
+            }
+
+            if (newOffset > DriveSize)
+                throw new ArgumentOutOfRangeException(nameof(offset), "Offset is past end of disk");
+
+            _currentOffset = newOffset;
+
+            return _currentOffset;
+        }
+
+        public override byte[] ReadFile(uint bytesToRead)
+        {
+            if (bytesToRead % 512 != 0)
+                throw new ArgumentException("Bytes to read must be multiple of 512", nameof(bytesToRead));
+
+            return InternalReadFile(bytesToRead, out uint _);
+        }
+
+        public override byte[] ReadFile(uint bytesToRead, out uint bytesRead)
+        {
+            if (bytesToRead % 512 != 0)
+                throw new ArgumentException("Bytes to read must be multiple of 512", nameof(bytesToRead));
+
+            return InternalReadFile(bytesToRead, out bytesRead);
+        }
+
+        public override byte[] ReadFile(uint bytesToRead, out uint bytesRead, ref NativeOverlapped overlapped)
+        {
+            if (bytesToRead % 512 != 0)
+                throw new ArgumentException("Bytes to read must be multiple of 512", nameof(bytesToRead));
+
+            var newOffset = Move((overlapped.OffsetHigh << 32) + overlapped.OffsetLow);
+
+            overlapped.OffsetHigh = (int) (newOffset >> 32);
+            overlapped.OffsetLow = (int) (newOffset & 0xFFFFFFFF);
+
+            return InternalReadFile(bytesToRead, out bytesRead);
+        }
+
+        public override byte[] SafeReadFile(uint bytesToRead)
+        {
+            var buffer = InternalReadFile(bytesToRead, out uint bytesRead);
+
+            Array.Resize(ref buffer, (int)bytesToRead);
+
+            return buffer;
+        }
+
+        private byte[] InternalReadFile(uint bytesToRead, out uint bytesRead)
+        {
+            var lcn = _currentOffset / (BytesPerSector * SectorsPerCluster);
+            var offsetInLcn = _currentOffset % (BytesPerSector * SectorsPerCluster);
+
+            if (!Parts.ContainsKey(lcn))
+            {
+                bytesRead = (uint)(BytesPerSector * SectorsPerCluster - offsetInLcn);
+                return new byte[bytesRead];
+            }
+
+            var clusterBytes = Parts[lcn].ReadAsCluster();
+
+            if (offsetInLcn == 0)
+            {
+                bytesRead = (uint)clusterBytes.Length;
+                return clusterBytes;
+            }
+            
+            var retBytes = new byte[bytesToRead];
+
+            Array.Copy(clusterBytes, (int)offsetInLcn, retBytes, 0, bytesToRead);
+
+            bytesRead = (uint) retBytes.Length;
+
+            return retBytes;
+        }
+
+        public override void Dispose()
+        {
+           
+        }
+    }
+}
---
+++ NtfsSharp.Tests/TestBootSector.cs
@@ -0,0 +1,140 @@
+﻿using NUnit.Framework;
+using System;
+using NtfsSharp.Exceptions;
+using NtfsSharp.Tests.Driver;
+
+namespace NtfsSharp.Tests
+{
+    [TestFixture]
+    public class TestBootSector
+    {
+        public const ushort BytesPerSector = 512;
+        public const byte SectorsPerCluster = 8;
+
+        private DummyDriver Driver { get; set; }
+        private Volume Volume { get; set; }
+        private BootSector BootSector { get; set; }
+
+        [SetUp]
+        public void SetUpDummyDisk()
+        {
+            Driver = new DummyDriver();
+            Volume = new Volume(Driver, false);
+            BootSector = new BootSector();
+
+            Driver.Parts[0] = BootSector;
+        }
+
+        [Test]
+        public void TestClustersPerMFTRecordPositive()
+        {
+            const byte clustersPerMftRecord = 0x7F;
+
+            BootSector.DummyBootSector.ClustersPerMFTRecord = clustersPerMftRecord;
+
+            Volume.ReadBootSector();
+
+            const int expected = clustersPerMftRecord * BytesPerSector * SectorsPerCluster;
+            Assert.AreEqual(expected, Volume.BytesPerFileRecord);
+            Assert.Greater(Volume.BytesPerFileRecord, 0);
+        }
+
+        [Test]
+        public void TestClustersPerMFTRecordNegativeValid()
+        {
+            var clustersPerMftRecord = unchecked((sbyte) 0xF6);
+
+            BootSector.DummyBootSector.ClustersPerMFTRecord = (byte) clustersPerMftRecord;
+
+            Volume.ReadBootSector();
+
+            var expected = (int) Math.Pow(2, Math.Abs(clustersPerMftRecord));
+            Assert.AreEqual(expected, Volume.BytesPerFileRecord);
+            Assert.Greater(Volume.BytesPerFileRecord, 0);
+        }
+
+        [Test]
+        public void TestClustersPerMFTRecordNegativeInvalid()
+        {
+            var clustersPerMftRecord = unchecked((sbyte)0x80);
+
+            BootSector.DummyBootSector.ClustersPerMFTRecord = (byte)clustersPerMftRecord;
+
+            var ex = Assert.Throws<InvalidBootSectorException>(Volume.ReadBootSector);
+            Assert.AreEqual(nameof(BootSector.DummyBootSector.ClustersPerMFTRecord), ex.FieldName);
+        }
+
+        [Test]
+        public void TestBytesPerSectorValid()
+        {
+            const ushort bytesPerSector = 512;
+
+            BootSector.DummyBootSector.BytesPerSector = bytesPerSector;
+
+            Volume.ReadBootSector();
+
+            Assert.AreEqual(bytesPerSector, Volume.BytesPerSector);
+            Assert.AreEqual(Volume.BytesPerFileRecord / Volume.BytesPerSector, Volume.SectorsPerMFTRecord);
+        }
+
+        [Test]
+        public void TestBytesPerSectorZero()
+        {
+            const ushort bytesPerSector = 0;
+
+            BootSector.DummyBootSector.BytesPerSector = bytesPerSector;
+
+            var ex = Assert.Throws<InvalidBootSectorException>(Volume.ReadBootSector);
+            Assert.AreEqual(nameof(BootSector.DummyBootSector.BytesPerSector), ex.FieldName);
+        }
+
+        [Test]
+        public void TestBytesPerSectorNotMultiple()
+        {
+            const ushort bytesPerSector = 511;
+
+            BootSector.DummyBootSector.BytesPerSector = bytesPerSector;
+
+            var ex = Assert.Throws<InvalidBootSectorException>(Volume.ReadBootSector);
+            Assert.AreEqual(nameof(BootSector.DummyBootSector.BytesPerSector), ex.FieldName);
+        }
+
+        [Test]
+        public void TestBytesPerSectorOverflow()
+        {
+            const ushort bytesPerSector = 8096;
+
+            BootSector.DummyBootSector.BytesPerSector = bytesPerSector;
+
+            var ex = Assert.Throws<InvalidBootSectorException>(Volume.ReadBootSector);
+            Assert.AreEqual(nameof(BootSector.DummyBootSector.BytesPerSector), ex.FieldName);
+        }
+
+        [Test]
+        public void TestSectorsPerClusterValid()
+        {
+            const byte sectorsPerCluster = 8;
+
+            BootSector.DummyBootSector.SectorsPerCluster = sectorsPerCluster;
+
+            Volume.ReadBootSector();
+
+            Assert.AreEqual(sectorsPerCluster, Volume.SectorsPerCluster);
+        }
+
+        [Test]
+        public void TestSectorsPerClusterMFTValid()
+        {
+            const byte sectorsPerCluster = 8;
+            const byte clustersPerMftRecord = 10;
+
+            BootSector.DummyBootSector.ClustersPerMFTRecord = clustersPerMftRecord;
+            BootSector.DummyBootSector.SectorsPerCluster = sectorsPerCluster;
+
+            Volume.ReadBootSector();
+
+            Assert.AreEqual(clustersPerMftRecord * sectorsPerCluster * BootSector.DummyBootSector.BytesPerSector,
+                Volume.BytesPerFileRecord);
+        }
+    }
+}
