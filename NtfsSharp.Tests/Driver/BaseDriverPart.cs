ee06fcc076c94a6037522609dbc7e4a7b794d409
Nick H <nick@little-apps.com>
2017-05-03 14:45:58 -0600

    Added NUnit tests

---
+++ NtfsSharp.Tests/Driver/BaseDriverPart.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NtfsSharp.Tests.Driver
+{
+    public abstract class BaseDriverPart
+    {
+        public BaseDriverPart()
+        {
+            GenerateDefaultDummy();
+        }
+
+        protected abstract void GenerateDefaultDummy();
+
+        public abstract byte[] BuildPart();
+
+        public byte[] ReadAsCluster()
+        {
+            var partBytes = BuildPart();
+
+            if (partBytes.Length == DummyDriver.BytesPerSector * DummyDriver.SectorsPerCluster)
+                return partBytes;
+
+            var clusterBytes = new byte[DummyDriver.BytesPerSector * DummyDriver.SectorsPerCluster];
+
+            Array.Copy(partBytes, 0, clusterBytes, 0, partBytes.Length);
+
+            return clusterBytes;
+        }
+
+        protected byte[] StructureToBytes<T>(T structure, uint size = 0) where T:struct
+        {
+            if (size == 0)
+                size = (uint) Marshal.SizeOf(structure);
+
+            var arr = new byte[size];
+            var ptr = Marshal.AllocHGlobal((int) size);
+
+            Marshal.StructureToPtr(structure, ptr, true);
+
+            Marshal.Copy(ptr, arr, 0, (int) size);
+
+            Marshal.FreeHGlobal(ptr);
+
+            return arr;
+        }
+    }
+}
---
+++ NtfsSharp.Tests/Driver/BootSector.cs
@@ -0,0 +1,76 @@
+﻿using System.Runtime.InteropServices;
+
+namespace NtfsSharp.Tests.Driver
+{
+    public class BootSector : BaseDriverPart
+    {
+        public NtfsBootSector DummyBootSector;
+
+        protected override void GenerateDefaultDummy()
+        {
+            DummyBootSector = new NtfsBootSector
+            {
+                JMPInstruction = new byte[] {0xeb, 0x52, 0x90},
+                OEMID = "NTFS    ".ToCharArray(),
+                BytesPerSector = (ushort) DummyDriver.BytesPerSector,
+                SectorsPerCluster = (byte) DummyDriver.SectorsPerCluster,
+                ReservedSectors = 0,
+                MediaDescriptor = Volume.Media.HardDrive,
+                SectorsPerTrack = 63,
+                NumberOfHeads = 255,
+                HiddenSectors = 0,
+                TotalSectors = DummyDriver.DriveSize / DummyDriver.BytesPerSector,
+                MFTLCN = DummyDriver.MasterFileTableLcn,
+                MFTMirrLCN = DummyDriver.MasterFileTableLcn,
+                ClustersPerMFTRecord = 246,
+                ClustersPerIndexBuffer = 1,
+                VolumeSerialNumber = 0x1234567890abcdef,
+                NTFSChecksum = 0,
+                BootStrapCode = new byte[426],
+                Signature = new byte[] {0xaa, 0x55}
+            };
+        }
+
+        public override byte[] BuildPart()
+        {
+            return StructureToBytes(DummyBootSector, DummyDriver.BytesPerSector * DummyDriver.SectorsPerCluster);
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        public struct NtfsBootSector
+        {
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+            public byte[] JMPInstruction;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
+            public char[] OEMID;
+            public ushort BytesPerSector;
+            public byte SectorsPerCluster;
+            public ushort ReservedSectors;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+            public byte[] AlwaysZero1;
+            public ushort NotUsed1;
+            public Volume.Media MediaDescriptor;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+            public byte[] AlwaysZero2;
+            public ushort SectorsPerTrack;
+            public ushort NumberOfHeads;
+            public uint HiddenSectors;
+            public uint NotUsed2;
+            public uint NotUsed3;
+            public ulong TotalSectors;
+            public ulong MFTLCN;
+            public ulong MFTMirrLCN;
+            public byte ClustersPerMFTRecord;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+            public byte[] NotUsed4;
+            public uint ClustersPerIndexBuffer;
+            public ulong VolumeSerialNumber;
+            public uint NTFSChecksum;
+
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 426)]
+            public byte[] BootStrapCode;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+            public byte[] Signature;
+        }
+    }
+}
