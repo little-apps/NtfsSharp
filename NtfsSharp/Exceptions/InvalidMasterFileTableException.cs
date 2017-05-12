c79749f64f098ae77a893277ac01bc6440ef8039
Nick H <nick@sameoldnick.com>
2017-05-12 09:44:26 -0600

    Added InvalidMasterFileTableException

---
+++ NtfsSharp/Exceptions/InvalidMasterFileTableException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NtfsSharp.FileRecords;
+
+namespace NtfsSharp.Exceptions
+{
+    public class InvalidMasterFileTableException : Exception
+    {
+        public readonly string ParamName;
+        public readonly FileRecord FileRecord;
+
+        public InvalidMasterFileTableException(string paramName, FileRecord fileRecord)
+        {
+            ParamName = paramName;
+            FileRecord = fileRecord;
+        }
+
+        public InvalidMasterFileTableException(string paramName, string message, FileRecord fileRecord) : base(message)
+        {
+            ParamName = paramName;
+            FileRecord = fileRecord;
+        }
+    }
+}
