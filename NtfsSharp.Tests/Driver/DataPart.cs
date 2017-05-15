e1bbd341264eb0c873a12bb91a7f63469c6c1a08
Nick H <nick@sameoldnick.com>
2017-05-14 19:55:30 -0600

    DataPart can be used to have cluster in byte form

---
+++ NtfsSharp.Tests/Driver/DataPart.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NtfsSharp.Tests.Driver
+{
+    class DataPart : BaseDriverPart
+    {
+        public byte[] Data { get; } = new byte[DummyDriver.BytesPerSector * DummyDriver.SectorsPerCluster];
+
+        protected override bool ShouldGenerateDefault
+        {
+            get { return false; }
+        }
+
+        protected override void GenerateDefaultDummy()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override byte[] BuildPart()
+        {
+            return Data;
+        }
+    }
+}
