a008971546828da1b74f32686fc6a4baa76a9d66
Nick H <nick@little-apps.com>
2017-05-28 23:37:31 -0600

    Added generic tests for resident and non-resident attributes

---
+++ NtfsSharp.Tests/Attributes/TestAttributes.cs
@@ -0,0 +1,126 @@
+﻿using NtfsSharp.Exceptions;
+using NtfsSharp.Tests.Driver;
+using NtfsSharp.Tests.Driver.Attributes;
+using NtfsSharp.Tests.Driver.Attributes.NonResident;
+using NUnit.Framework;
+
+namespace NtfsSharp.Tests.Attributes
+{
+    [TestFixture]
+    public class TestAttributes : TestAttributesBase
+    {
+        public static DummyAttributeBase.NTFS_ATTR_TYPE[] ResidentTypes =
+        {
+            DummyAttributeBase.NTFS_ATTR_TYPE.BITMAP,
+            DummyAttributeBase.NTFS_ATTR_TYPE.DATA,
+            DummyAttributeBase.NTFS_ATTR_TYPE.EA,
+            DummyAttributeBase.NTFS_ATTR_TYPE.EA_INFORMATION,
+            DummyAttributeBase.NTFS_ATTR_TYPE.FILE_NAME,
+            DummyAttributeBase.NTFS_ATTR_TYPE.LOGGED_UTILITY_STREAM,
+            DummyAttributeBase.NTFS_ATTR_TYPE.OBJECT_ID,
+            DummyAttributeBase.NTFS_ATTR_TYPE.REPARSE_POINT,
+            DummyAttributeBase.NTFS_ATTR_TYPE.SECURITY_DESCRIPTOR,
+            DummyAttributeBase.NTFS_ATTR_TYPE.STANDARD_INFORMATION,
+            DummyAttributeBase.NTFS_ATTR_TYPE.VOLUME_INFORMATION,
+            DummyAttributeBase.NTFS_ATTR_TYPE.VOLUME_NAME
+        };
+
+        public static DummyAttributeBase.NTFS_ATTR_TYPE[] NonResidentTypes =
+        {
+            DummyAttributeBase.NTFS_ATTR_TYPE.BITMAP,
+            DummyAttributeBase.NTFS_ATTR_TYPE.DATA,
+            DummyAttributeBase.NTFS_ATTR_TYPE.EA,
+            DummyAttributeBase.NTFS_ATTR_TYPE.EA_INFORMATION,
+            DummyAttributeBase.NTFS_ATTR_TYPE.LOGGED_UTILITY_STREAM,
+            DummyAttributeBase.NTFS_ATTR_TYPE.OBJECT_ID,
+            DummyAttributeBase.NTFS_ATTR_TYPE.REPARSE_POINT,
+            DummyAttributeBase.NTFS_ATTR_TYPE.SECURITY_DESCRIPTOR,
+            DummyAttributeBase.NTFS_ATTR_TYPE.VOLUME_INFORMATION
+        };
+
+        [Test]
+        public void TestResidentAttribute([ValueSource(nameof(ResidentTypes))] DummyAttributeBase.NTFS_ATTR_TYPE attrType)
+        {
+            var residentAttr = new ResidentDummy(attrType);
+
+            DummyFileRecord.Attributes.Add(residentAttr);
+
+            var actualFileRecord = ReadDummyFileRecord();
+
+            Assert.AreEqual(1, actualFileRecord.Attributes.Count);
+
+            var actualAttribute = actualFileRecord.Attributes[0];
+            var actualBody = actualAttribute.Body.Body ?? actualAttribute.Header.ReadBody();
+
+            Assert.AreEqual((uint)attrType, (uint)actualAttribute.Header.Header.Type);
+            Assert.False(actualAttribute.Header.Header.NonResident);
+            Assert.AreEqual(actualBody.Length, actualBody.Length);
+        }
+
+        [Test]
+        public void TestInvalidResidentAttributeType()
+        {
+            var residentAttr = new ResidentDummy(0);
+
+            DummyFileRecord.Attributes.Add(residentAttr);
+
+            Assert.Throws<InvalidAttributeException>(() => ReadDummyFileRecord());
+        }
+
+        [Test]
+        public void TestNonResidentAttribute([ValueSource(nameof(NonResidentTypes))] DummyAttributeBase.NTFS_ATTR_TYPE attrType)
+        {
+            var nonResidentAttr = new NonResidentDummy(attrType);
+            var dataCluster = new DataCluster();
+
+            nonResidentAttr.AdditionalClusters.Add(100, dataCluster);
+
+            DummyFileRecord.Attributes.Add(nonResidentAttr);
+
+            var actualFileRecord = ReadDummyFileRecord();
+
+            Assert.AreEqual(1, actualFileRecord.Attributes.Count);
+
+            var actualAttribute = actualFileRecord.Attributes[0];
+            var actualBody = actualAttribute.Body.Body ?? actualAttribute.Header.ReadBody();
+
+            Assert.AreEqual((uint) attrType, (uint) actualAttribute.Header.Header.Type);
+            Assert.True(actualAttribute.Header.Header.NonResident);
+            Assert.AreEqual(dataCluster.Data.Length, actualBody.Length);
+        }
+
+        [Test]
+        public void TestInvalidNonResidentAttributeType()
+        {
+            var nonResidentAttr = new NonResidentDummy(0);
+
+            DummyFileRecord.Attributes.Add(nonResidentAttr);
+
+            Assert.Throws<InvalidAttributeException>(() => ReadDummyFileRecord());
+        }
+    }
+
+    public class ResidentDummy : ResidentAttributeBase
+    {
+        public readonly byte[] Body;
+
+        public ResidentDummy(NTFS_ATTR_TYPE type)
+        {
+            Header.Type = type;
+            Body = new byte[128];
+        }
+
+        protected override byte[] GetBody()
+        {
+            return Body;
+        }
+    }
+
+    public class NonResidentDummy : NonResidentAttributeBase
+    {
+        public NonResidentDummy(NTFS_ATTR_TYPE type)
+        {
+            Header.Type = type;
+        }
+    }
+}
