0853a821cfb09ee3eb9e6de381440f065d4c2bab
Nick H <nick@sameoldnick.com>
2017-05-28 23:34:57 -0600

    Byteable abstract class is used to convert structs to byte arrays in its child classes

--- NtfsSharp.Tests/Driver/BaseDriverCluster.cs
+++ NtfsSharp.Tests/Driver/BaseDriverCluster.cs
@@ -3,12 +3,12 @@ using System.Runtime.InteropServices;
 
 namespace NtfsSharp.Tests.Driver
 {
-    public abstract class BaseDriverCluster
+    public abstract class BaseDriverCluster : Byteable
     {
         public BaseDriverCluster()
         {
         }
-
+        
         protected abstract bool ShouldGenerateDefault { get; }
 
         /// <summary>
@@ -43,31 +43,6 @@ namespace NtfsSharp.Tests.Driver
             Array.Copy(partBytes, 0, clusterBytes, 0, partBytes.Length);
 
             return clusterBytes;
-        }
-
-        /// <summary>
-        /// Converts a structure into it's bytes
-        /// </summary>
-        /// <typeparam name="T">Structure type</typeparam>
-        /// <param name="structure">Structure to convert</param>
-        /// <param name="size">Number of bytes to be return. If zero, the <seealso cref="Marshal"/>.SizeOf of the structure is used. (default: 0)</param>
-        /// <returns>Byte array containing structure</returns>
-        /// <remarks>If the size is bigger than the actual structure, any extra bytes will be 0x00.</remarks>
-        protected byte[] StructureToBytes<T>(T structure, uint size = 0) where T:struct
-        {
-            if (size == 0)
-                size = (uint) Marshal.SizeOf(structure);
-
-            var arr = new byte[size];
-            var ptr = Marshal.AllocHGlobal((int) size);
-
-            Marshal.StructureToPtr(structure, ptr, true);
-
-            Marshal.Copy(ptr, arr, 0, (int) size);
-
-            Marshal.FreeHGlobal(ptr);
-
-            return arr;
         }
     }
 }
---
+++ NtfsSharp.Tests/Driver/Byteable.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.InteropServices;
+
+namespace NtfsSharp.Tests.Driver
+{
+    public abstract class Byteable
+    {
+        /// <summary>
+        /// Converts a structure into it's bytes
+        /// </summary>
+        /// <typeparam name="T">Structure type</typeparam>
+        /// <param name="structure">Structure to convert</param>
+        /// <param name="size">Number of bytes to be return. If zero, the <seealso cref="Marshal"/>.SizeOf of the structure is used. (default: 0)</param>
+        /// <returns>Byte array containing structure</returns>
+        /// <remarks>If the size is bigger than the actual structure, any extra bytes will be 0x00.</remarks>
+        protected static byte[] StructureToBytes<T>(T structure, uint size = 0) where T : struct
+        {
+            if (size == 0)
+                size = (uint)Marshal.SizeOf(structure);
+
+            var arr = new byte[size];
+            var ptr = Marshal.AllocHGlobal((int)size);
+
+            Marshal.StructureToPtr(structure, ptr, true);
+
+            Marshal.Copy(ptr, arr, 0, (int)size);
+
+            Marshal.FreeHGlobal(ptr);
+
+            return arr;
+        }
+    }
+}
