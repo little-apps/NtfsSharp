6782a7634b77251b7dfbb21c80c2ac5bbf9bb35a
Nick H <nick@sameoldnick.com>
2017-06-26 22:34:58 -0600

    Only uses Mono for Travis CI

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ "$DOTNET" = "true" ]; then
-	dotnet restore
-fi
-
-if [ "$MONO" = "true" ]; then
-	nuget restore NtfsSharp.sln
-	nuget install NUnit.ConsoleRunner -Version 3.6.1 -OutputDirectory testrunner
-fi
