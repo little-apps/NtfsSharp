0fc0c706eee52214946e663488fda71e9dafa9bb
Nick H <nick@little-apps.com>
2017-10-01 16:10:41 -0600

    Removed whitespace

--- NtfsSharp/FileRecords/Attributes/AttributeList/AttributeList.cs
+++ NtfsSharp/FileRecords/Attributes/AttributeList/AttributeList.cs
@@ -7,7 +7,7 @@ using NtfsSharp.FileRecords.Attributes.MetaData;
 namespace NtfsSharp.FileRecords.Attributes.AttributeList
 {
     /// <summary>
-    /// When there are lots of attributes and space in the MFT record is short, all those attributes that can be made non-resident are moved out of the MFT. If there is still not enough room, then an $ATTRIBUTE_LIST attribute is needed. The remaining attributes are placed in a new MFT record and the $ATTRIBUTE_LIST describes where to find them. It is very unusual to see this attribute. 
+    /// When there are lots of attributes and space in the MFT record is short, all those attributes that can be made non-resident are moved out of the MFT. If there is still not enough room, then an $ATTRIBUTE_LIST attribute is needed. The remaining attributes are placed in a new MFT record and the $ATTRIBUTE_LIST describes where to find them. It is very unusual to see this attribute.
     /// </summary>
     [Resident(AttributeHeaderBase.NTFS_ATTR_TYPE.ATTRIBUTE_LIST)]
     [NonResident(AttributeHeaderBase.NTFS_ATTR_TYPE.ATTRIBUTE_LIST)]
