a841887fd296db0c17a3ed54ec73c3b83227ca4e
Nick H <nick@sameoldnick.com>
2018-03-04 22:05:47 -0700

    MasterBootRecord and GuidPartitionTable works with not just the PhysicalDiskDriver

---
+++ NtfsSharp.Drivers/Physical/Partition.cs
@@ -0,0 +1,24 @@
+﻿namespace NtfsSharp.Drivers.Physical
+{
+    public class Partition
+    {
+        /// <summary>
+        /// The starting sector of the selected partition
+        /// </summary>
+        public ulong StartSector { get; }
+
+        /// <summary>
+        /// The last sector (from the start of the drive) of the selected partition
+        /// </summary>
+        public ulong EndSector { get; }
+
+        public MasterBootRecord Mbr { get; }
+
+        public Partition(ulong startSector, ulong endSector, MasterBootRecord masterBootRecord)
+        {
+            StartSector = startSector;
+            EndSector = endSector;
+            Mbr = masterBootRecord;
+        }
+    }
+}
