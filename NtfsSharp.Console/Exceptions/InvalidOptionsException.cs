81318876cfe51ee738fc506af9701319dc3fc51e
Nick H <nick@little-apps.com>
2019-07-20 01:22:23 -0600

    Fixed option validation

---
+++ NtfsSharp.Console/Exceptions/InvalidOptionsException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NtfsSharp.Console.Exceptions
+{
+    internal class InvalidOptionsException : Exception
+    {
+        internal InvalidOptionsException(string message) : base(message)
+        {
+
+        }
+
+        internal InvalidOptionsException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+    }
+}
