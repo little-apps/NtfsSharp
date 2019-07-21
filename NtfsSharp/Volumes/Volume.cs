b5d0945cbe5021e31f36834aba45c6cf2810c1bc
Nick H <nick@little-apps.com>
2019-07-20 19:10:23 -0600

    Constructor takes instance of IDiskDriver

--- NtfsSharp/Volumes/Volume.cs
+++ NtfsSharp/Volumes/Volume.cs
@@ -55,7 +55,7 @@ namespace NtfsSharp.Volumes
         /// <param name="diskDriver">Disk driver to use to read the Volume.</param>
         /// <exception cref="ArgumentNullException">Thrown if <paramref name="diskDriver"/> is null.</exception>
         /// <remarks>The <seealso cref="Read"/> method is not called when a <see cref="Volume"/> object is created. This must be done by the calling method.</remarks>
-        public Volume(BaseDiskDriver diskDriver)
+        public Volume(IDiskDriver diskDriver)
         {
             Driver = diskDriver ?? throw new ArgumentNullException(nameof(diskDriver));
         }
