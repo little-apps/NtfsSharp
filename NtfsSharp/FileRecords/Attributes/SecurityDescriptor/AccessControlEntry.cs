096605311519a77277f45924323a3f0fa7423f4c
Nick H <nick@little-apps.com>
2020-06-18 00:20:19 -0600

    Reads ACLs, ACEs, and SIDs from Security Descriptor attributes.

---
+++ NtfsSharp/FileRecords/Attributes/SecurityDescriptor/AccessControlEntry.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace NtfsSharp.FileRecords.Attributes.SecurityDescriptor
+{
+    /// <summary>
+    /// Access control entries (ACE) are entries in an access control list containing
+    /// information describing the access rights related to a particular security
+    /// identifier or user. Each access control entry contains an ID, which identifies
+    /// the subject group or individual.
+    /// </summary>
+    public class AccessControlEntry
+    {
+        /// <summary>
+        /// Access Control Entry Header
+        /// </summary>
+        public ACEHeader Header { get; }
+
+        /// <summary>
+        /// Security identifier that access control entry applies to.
+        /// </summary>
+        public SecurityIdentifier SID { get; }
+
+        public AccessControlEntry(ACEHeader header, SecurityIdentifier sid)
+        {
+            Header = header;
+            SID = sid;
+        }
+
+        public struct ACEHeader
+        {
+            public readonly byte Type;
+            public readonly ACEFlags Flags;
+            public readonly byte Size;
+            public readonly AccessMask AccessMask;
+        }
+
+        [Flags]
+        public enum ACEFlags : byte
+        {
+            AccessAllowed = 0,
+            AccessDenied = 1,
+            SystemAudit = 2
+        }
+
+        [Flags]
+        public enum AccessMask : uint
+        {
+            FileReadData = 0x0001,
+            FileListDirectory = 0x0001,
+            FileWriteData = 0x0002,
+            FileAddFile = 0x0002,
+            FileAppendData = 0x0004,
+            FileAddSubDirectory = 0x0004,
+            FileCreatePipeInstance = 0x0004,
+            FileReadEa = 0x0008,
+            FileWriteEa = 0x0010,
+            FileExecute = 0x0020,
+            FileTraverse = 0x0020,
+            FileDeleteChild = 0x0040,
+            FileReadAttributes = 0x0080,
+            FileWriteAttributes = 0x0100,
+
+            ReadControl = 0x00020000,
+
+            StandardRightsRequired = 0x000F0000,
+            StandardRightsRead = ReadControl,
+            StandardRightsWrite = ReadControl,
+            StandardRightsExecute = ReadControl,
+            Synchronize = 0x00100000,
+
+            FileAllAccess = StandardRightsRequired | Synchronize | 0x1FF,
+            FileGenericRead = StandardRightsRead | FileReadData | FileReadAttributes | FileReadEa | Synchronize,
+            FileGenericWrite = StandardRightsWrite | FileWriteData | FileWriteAttributes | FileWriteEa | FileAppendData | Synchronize,
+            FileGenericExecute = StandardRightsExecute | FileReadAttributes | FileExecute | Synchronize
+        }
+    }
+}
---
+++ NtfsSharp/FileRecords/Attributes/SecurityDescriptor/SecurityIdentifier.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using NtfsSharp.Helpers;
+using SID = System.Security.Principal.SecurityIdentifier;
+
+namespace NtfsSharp.FileRecords.Attributes.SecurityDescriptor
+{
+    /// <summary>
+    /// A data structure of variable length that identifies user, group, and
+    /// computer accounts. Every account on a network is issued a unique SID
+    /// when the account is first created. Internal processes in Windows refer
+    /// to an account's SID rather than the account's user or group name.
+    /// </summary>
+    public sealed class SecurityIdentifier : IComparable<SecurityIdentifier>
+    {
+        public byte Revision { get; }
+        public byte SubAuthorityCount { get; }
+        public byte[] NtAuthority { get; }
+        public uint[] SubAuthorities { get; }
+
+        /// <summary>
+        /// The <seealso cref="System.Security.Principal.SecurityIdentifier"/> instance for the SID.
+        /// </summary>
+        public SID SID { get; }
+
+        /// <summary>
+        /// Creates an instance of <see cref="SecurityIdentifier"/> using the raw bytes.
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="bytes"/> is null.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="bytes"/> length is less than 8.</exception>
+        public SecurityIdentifier(byte[] bytes)
+        {
+            if (bytes == null)
+                throw new ArgumentNullException(nameof(bytes));
+
+            if (bytes.Length < 8)
+                throw new ArgumentOutOfRangeException(nameof(bytes), "Must be at least 8 bytes to parse SID.");
+
+            Revision = bytes[0];
+            SubAuthorityCount = bytes[1];
+
+            NtAuthority = bytes.GetBytesAtOffset(2, 6);
+
+            SubAuthorities = new uint[SubAuthorityCount];
+
+            for (var sidPart = 0; sidPart < SubAuthorityCount; sidPart++)
+            {
+                var partStartOffset = 8 + sidPart * 4;
+
+                SubAuthorities[sidPart] = BitConverter.ToUInt32(bytes, partStartOffset);
+            }
+
+            SID = new SID(bytes, 0);
+        }
+
+        public int CompareTo(SecurityIdentifier other)
+        {
+            return SID.CompareTo(other.SID);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return SID.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return SID.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return SID.ToString();
+        }
+
+        /// <summary>
+        /// Create <see cref="SecurityIdentifier"/> from offset in bytes.
+        /// </summary>
+        /// <param name="bytes">Bytes containing SID.</param>
+        /// <param name="offset">Offset of SID in bytes.</param>
+        /// <returns>Instance of <seealso cref="SecurityIdentifier"/></returns>
+        public static SecurityIdentifier MakeFromBytes(byte[] bytes, uint offset)
+        {
+            var subAuthorityCount = bytes[offset + 1];
+
+            var sidSize = 8 + ((uint) subAuthorityCount * 4);
+            var sidBytes = bytes.GetBytesAtOffset(offset, sidSize);
+
+            return new SecurityIdentifier(sidBytes);
+        }
+    }
+}
