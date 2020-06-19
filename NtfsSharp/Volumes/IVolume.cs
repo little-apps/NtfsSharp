016d1f8a42ef19ea80f3fcd85dfa426fc13bc20e
Nick H <nick@sameoldnick.com>
2020-06-18 23:33:19 -0600

    Removed NTFS related items from IVolume

--- NtfsSharp/Volumes/IVolume.cs
+++ NtfsSharp/Volumes/IVolume.cs
@@ -1,21 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using NtfsSharp.FileRecords;
 using NtfsSharp.Units;
 
 namespace NtfsSharp.Volumes
 {
     public interface IVolume : IComparable<IVolume>
     {
-        IDiskDriver Driver { get; }
 
-        ulong MftLcn { get; }
 
         uint SectorsPerCluster { get; }
         ushort BytesPerSector { get; }
-        uint SectorsPerMftRecord { get; }
 
-        IReadOnlyDictionary<uint, FileRecord> MFT { get; }
 
         IVolume Read();
         Cluster ReadLcn(ulong lcn);
